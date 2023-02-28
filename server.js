2b8bab225da7588fef2e685fe2a55c7c59b22b08
John Lai <lai.john.py@gmail.com>
2023-02-28 13:29:09 -0500

    Add boilerplate set up for server.js

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
-const express = require('express')
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
-const app = express()
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const PORT = 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create();
 
-app.listen(() => {console.log(`Server is listening ${PORT}`)})
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
