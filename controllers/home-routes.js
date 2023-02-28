920f63a9fa67460b500bf3af74622414917f1aa4
John Lai <lai.john.py@gmail.com>
2023-02-28 13:59:12 -0500

    Ad homepage view

---
+++ controllers/home-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+//Get router for home page
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', {});
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
