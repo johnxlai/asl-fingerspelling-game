6c03f708cadbce7a3879d6d8d3d70d8d3ba657ab
John Lai <lai.john.py@gmail.com>
2023-03-01 10:45:02 -0500

    Add correct path to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./views/layouts/main.handlebars'],
-  // './views/**/*/{handlebars}',
+  content: ['./views/**/*.{handlebars,html}'],
   theme: {
     extend: {},
   },
