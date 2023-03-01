1f5140361c17060b69a99611e3ad1b4c23a626a9
John Lai <lai.john.py@gmail.com>
2023-03-01 13:38:36 -0500

    update home route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-
+const { User } = require('../models');
 //Get router for home page
 router.get('/', async (req, res) => {
   try {
@@ -12,11 +12,32 @@ router.get('/', async (req, res) => {
 
 //Get Router for Start Game page
 router.get('/start', async (req, res) => {
-  try {
-    res.render('start', {});
-  } catch (err) {
-    res.status(500).json(err);
-  }
+  // try {
+  const userData = await User.findAll({
+    attributes: ['username'],
+  });
+
+  // const user = userData.get({ plain: true });
+
+  console.log(userData);
+
+  // Get all projects and JOIN with user data
+  // const projectData = await Project.findAll({
+  //   include: [
+  //     {
+  //       model: User,
+  //       attributes: ['name'],
+  //     },
+  //   ],
+  // });
+
+  res.render('start', {
+    userData,
+  });
+
+  // } catch (err) {
+  // res.status(500).json(err);
+  // }
 });
 
 module.exports = router;
