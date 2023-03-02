a6f6e10b24b6fdab486ff9b4437776f95266901b
John Lai <lai.john.py@gmail.com>
2023-03-02 09:58:38 -0500

    Grabbing all users

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -13,13 +13,11 @@ router.get('/', async (req, res) => {
 //Get Router for Start Game page
 router.get('/start', async (req, res) => {
   // try {
-  const userData = await User.findAll({
-    attributes: ['username'],
-  });
+  const userData = await User.findAll();
 
-  // const user = userData.get({ plain: true });
+  const gamer = (userData = userData.map((user) => user.get({ plain: true })));
 
-  console.log(userData);
+  console.log(gamer);
 
   // Get all projects and JOIN with user data
   // const projectData = await Project.findAll({
