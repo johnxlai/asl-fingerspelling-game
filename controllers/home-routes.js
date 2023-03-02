84a94a67b69777177e26b7d45f054262adf9adcf
John Lai <lai.john.py@gmail.com>
2023-03-02 14:12:17 -0500

    Add route to profile view, remove extra js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,10 @@ const { User, Result } = require('../models');
 //Get router for home page
 router.get('/', async (req, res) => {
   try {
-    res.render('homepage', {loggedIn: req.session.loggedIn, user: req.session.user});
+    res.render('homepage', {
+      loggedIn: req.session.loggedIn,
+      user: req.session.user,
+    });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
@@ -36,6 +39,7 @@ router.get('/start', async (req, res) => {
   // }
 });
 
+//Ranking and highscore page
 router.get('/ranks', async (req, res) => {
   const usersData = await User.findAll({
     attributes: { exclude: ['password'] },
@@ -47,4 +51,18 @@ router.get('/ranks', async (req, res) => {
   res.render('ranks', { users });
 });
 
+// Profile page (with Auth)
+router.get('/profile', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Result, attributes: ['points'] }],
+    });
+
+    const user = userData.get({ plain: true });
+    res.render('profile', { ...user });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
