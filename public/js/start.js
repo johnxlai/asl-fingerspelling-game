2099df041975c58d2983dd3a904a9c160738817d
Fabitheguy003 <fabienmukungu@gmail.com>
2023-03-03 13:28:51 -0500

    fornt end

--- public/js/start.js
+++ public/js/start.js
@@ -1,4 +1,4 @@
-      // Define an object that maps each letter of the alphabet to an image filename
+// Define an object that maps each letter of the alphabet to an image filename
       const letterImages = {
         'a': 'image/alphabet/a.svg',
         'b': 'image/alphabet/b.svg',
@@ -28,158 +28,98 @@
         'z': 'image/alphabet/z.svg'
       };
       
+      // Define an array of words to use for the game, with each word assigned a level
+      const words = [
+        { word: "love", level: 1 },
+        { word: "joke", level: 2 },
+        { word: "feel", level: 3 },
+        { word: "Cook", level: 4 },
+        { word: "baby", level: 5 }
+      ];
       
-    // Define an array of words to use for the game, with each word assigned a level
-    const words = [
-      { word: "love", level: 1 },
-      { word: "joke", level: 2 },
-      { word: "feel", level: 3 },
-      { word: "Cook", level: 4 },
-      { word: "baby", level: 5 }
-    ];
-    
-    let currentScore = 0;
-    let currentLevel = 1;
-    const maxLevel = 5; // set the maximum level
-    
-    // Initialize the flags for each level
-let level1_unlocked = true;
-let level2_unlocked = false;
-let level3_unlocked = false;
-let level4_unlocked = false;
-let level5_unlocked = false;
+      
+// Define a variable to keep track of the current word index
+let currentWordIndex = 0;
 
-// Define a function to check if a level is unlocked
-function is_level_unlocked(level) {
-  if (level === 1) {
-    return level1_unlocked;
-  } else if (level === 2) {
-    return level2_unlocked;
-  } else if (level === 3) {
-    return level3_unlocked;
-  } else if (level === 4) {
-    return level4_unlocked;
-  } else if (level === 5) {
-    return level5_unlocked;
-  } else {
-    return false;
-  }
-}
+// Define a variable to keep track of the highest level completed
+let highestLevelCompleted = 1;
 
-// Define a function to unlock the next level
-function unlock_next_level(level) {
-  if (level === 1) {
-    level2_unlocked = true;
-  } else if (level === 2) {
-    level3_unlocked = true;
-  } else if (level === 3) {
-    level4_unlocked = true;
-  } else if (level === 4) {
-    level5_unlocked = true;
-  }
-} else if (level === 5) {
-    level5_unlocked = true;
+// Function to display a word and its letters
+function displayWord(word) {
+  // Split the word into an array of letters
+  const letters = word.split('');
+
+  // Loop through the letters array and set the src attribute of each image to the corresponding letter image
+  for (let i = 0; i < letters.length; i++) {
+    const image = document.getElementById('image-' + i);
+    image.src = letterImages[letters[i].toLowerCase()];
+    image.alt = 'letter ' + i;
   }
 
-// Example usage
-if (is_level_unlocked(1)) {
-  // Play level 1
-  unlock_next_level(1);
+  // Update the level display
+  const levelDisplay = document.getElementById('level-display');
+  levelDisplay.textContent = words[currentWordIndex].level;
 }
-if (is_level_unlocked(2)) {
-  // Play level 2
-  unlock_next_level(2);
-}
-// And so on for the other levels...
 
-    
-    function getRandomWord() {
-      const filteredWords = words.filter(wordObj => wordObj.level === currentLevel);
-      const randomIndex = Math.floor(Math.random() * filteredWords.length);
-      return filteredWords[randomIndex].word;
+// Display the first word
+displayWord(words[currentWordIndex].word);
+
+// Add an event listener to the form submit button
+const form = document.getElementById('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  // Get the user input value
+  const userInput = document.getElementById('input').value.trim().toLowerCase();
+
+  // Check if the user input matches the current word
+  if (userInput === words[currentWordIndex].word.toLowerCase()) {
+    // Increment the current word index
+    currentWordIndex++;
+
+    // Check if all words have been displayed
+    if (currentWordIndex >= words.length) {
+      // Display a message and reset the current word index
+      alert('You have completed all the words!');
+      currentWordIndex = 0;
     }
-    
-    function updateScoreAndLevel() {
-      document.getElementById("score").textContent = currentScore;
-      document.getElementById("level-display").textContent = currentLevel;
-    }
-    
-    function updateLetterImages() {
-      const word = getRandomWord();
-      for (let i = 0; i < word.length; i++) {
-        const letter = word[i];
-        const imageSrc = letterImages[letter];
-        document.getElementById(`image-${i}`).src = imageSrc;
+
+    // Check if the current level is unlocked
+    if (words[currentWordIndex].level <= highestLevelCompleted + 1) {
+      // Display the next word
+      displayWord(words[currentWordIndex].word);
+
+      // Check if the current word is in level 1 and the user's answer is correct
+      if (words[currentWordIndex].level === 1 && userInput === words[currentWordIndex].word.toLowerCase()) {
+        // Unlock level 2 by setting the highestLevelCompleted variable to 1
+        highestLevelCompleted = 1;
       }
-    }
-    
-    function unlockNextLevel() {
-      if (currentLevel < maxLevel && currentScore >= currentLevel * 2) {
-        currentLevel++;
-        updateScoreAndLevel();
+
+      // Update the highest level completed if necessary
+      if (words[currentWordIndex].level > highestLevelCompleted) {
+        highestLevelCompleted = words[currentWordIndex].level;
       }
-    }
-    
-    function handleFormSubmit(event) {
-      event.preventDefault();
-    
-      const currentWord = getRandomWord(); // get the current word
-      const userAnswer = document.getElementById("input").value.toLowerCase();
-      const feedback = document.getElementById("feedback");
-    
-      if (userAnswer === currentWord) { // check if the user's answer is correct
-        currentScore = currentScore + 1; // increment the score
-        updateLetterImages(); // update the letter images
-        unlockNextLevel(); // check if the player has unlocked the next level
-        updateScoreAndLevel(); // update the score and level
-        feedback.textContent = "Correct!"; // display a message to the user that the answer is correct
-      } else {
-        feedback.textContent = "Incorrect!"; // display a message to the user that the answer is incorrect
-      }
-    
-      // clear the input field and prepare for the next word
-      document.getElementById("input").value = "";
-    
-      // wait for 1 second before proceeding to the next word
-      setTimeout(() => {
-        feedback.textContent = "";
-        // generate a new random word and display it to the user
-        const newWord = getRandomWord();
-        // you can display the new word here or in another function
-      }, 1000);
-    }
-    
-    const feedback = document.getElementById("feedback");
-    
-    const feedbackElement = document.getElementById("feedback");
-    if (feedbackElement !== null) {
-      feedbackElement.textContent = "Correct!";
+
+      // Update the points display
+      const pointsDisplay = document.getElementById('points-display');
+      pointsDisplay.textContent = parseInt(pointsDisplay.textContent) + 1;
+
+      // Clear the feedback message
+      const feedback = document.getElementById('feedback');
+      feedback.textContent = '';
     } else {
-      // Handle the case where the element does not exist
+      // Display an error message
+      const feedback = document.getElementById('feedback');
+      feedback.textContent = 'Level ' + words[currentWordIndex].level + ' is locked. Complete level ' + (words[currentWordIndex].level - 1) + ' first.';
     }
-    
-    
-    
-    function goToNextLevel() {
-      // Check if the player has reached the maximum level
-      if (currentLevel >= maxLevel) {
-        // Display a message to the player that they have won the game
-        alert("Congratulations, you have completed all levels!");
-      } else {
-        // Update the current level and generate a new random word
-        currentLevel++;
-        const newWord = getRandomWord();
-        // Update the UI to reflect the new level and word
-        updateScoreAndLevel();
-        updateLetterImages();
-        document.getElementById("input").value = "";
-        // Display the new word to the user
-        document.getElementById("word-display").textContent = newWord;
-      }
-    }
-    
-    
-    updateScoreAndLevel();
-    updateLetterImages();
-    
-    document.getElementById("form").addEventListener("submit", handleFormSubmit);
+  } else {
+    // Display an error message
+    const feedback = document.getElementById('feedback');
+    feedback.textContent = 'Incorrect. Try again. guess right to unlock next level';
+  }
+
+  // Clear the user input field
+  const input = document.getElementById('input');
+  input.value = '';
+});
+
