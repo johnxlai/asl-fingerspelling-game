6485bedce05499ab89721435ec52087721078502
John Lai <lai.john.py@gmail.com>
2023-03-03 20:10:16 -0500

    Add index route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-const resultRoutes = require('./result-routes');
+const resultRoutes = require('./result-routes.js');
 
 router.use('/users', userRoutes);
 router.use('/results', resultRoutes);
