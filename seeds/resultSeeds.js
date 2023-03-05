a436db4d4fe7cc17c1d901c0ce289a218ab7220c
John Lai <lai.john.py@gmail.com>
2023-03-05 16:23:36 -0500

    Add superuser icon to all page and add new seeds

--- seeds/resultSeeds.js
+++ seeds/resultSeeds.js
@@ -27,6 +27,30 @@ const resultData = [
     user_id: 2,
     points: 10,
   },
+  {
+    user_id: 4,
+    points: 1500,
+  },
+  {
+    user_id: 5,
+    points: 50,
+  },
+  {
+    user_id: 6,
+    points: 500,
+  },
+  {
+    user_id: 7,
+    points: 200,
+  },
+  {
+    user_id: 8,
+    points: 100,
+  },
+  {
+    user_id: 8,
+    points: 50,
+  },
 ];
 
 module.exports = resultData;
