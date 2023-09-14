c1894e68b8adea7760d4eb844e6e32708568394a
John Lai <lai.john.py@gmail.com>
2023-09-14 13:25:56 -0400

    Add alphabet array

--- public/js/practice.js
+++ public/js/practice.js
@@ -1,5 +1,21 @@
+// generate a to z, 26 elements, Starts are Character Code at 65(uppercase A) 97 (lowercase) and give it 0 as index
+const alphabetArray = Array.from({ length: 26 }, (_, index) =>
+  String.fromCharCode(97 + index)
+);
+console.log(alphabetArray);
+
 //Selectors
 
 // functions
 
 // Add event listeners
+
+// start button initialiate game start
+
+// start 30 secs count down
+
+// display first letter
+
+// input listen to event for correct or wrong answer
+
+// from a to z
