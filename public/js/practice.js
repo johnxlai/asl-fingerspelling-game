4f9f9ea36aff87d04a76fdceb856d08b0e44d477
John Lai <lai.john.py@gmail.com>
2023-09-15 11:51:14 -0400

    Add message

--- public/js/practice.js
+++ public/js/practice.js
@@ -1,19 +1,27 @@
-// generate a to z, 26 elements, Starts are Character Code at 65(uppercase A) 97 (lowercase) and give it 0 as index
-let alphabetArray = Array.from({ length: 26 }, (_, index) =>
-  String.fromCharCode(97 + index)
-);
-
-//randomize the array
-alphabetArray.sort((a, b) => 0.5 - Math.random());
-
-alphabetArray = ['a', 'b', 'c'];
 //Selectors
+let alphabetArray = [];
 const startBtn = document.querySelector('.start-practice');
 const gameSection = document.querySelector('.game-section');
 const img = document.querySelector('.question');
 const gameForm = document.querySelector('.game-form');
 const userInput = document.querySelector('.user-input');
 const endGameEl = document.querySelector('.end-game');
+const feedback = document.querySelector('.feedback');
+
+//Start Game, create alphabet and randomize
+function startGame() {
+  // generate a to z, 26 elements, Starts are Character Code at 65(uppercase A) 97 (lowercase) and give it 0 as index
+  alphabetArray = Array.from({ length: 26 }, (_, index) =>
+    String.fromCharCode(97 + index)
+  );
+
+  //randomize the array
+  alphabetArray.sort((a, b) => 0.5 - Math.random());
+  alphabetArray = ['a', 'b', 'c'];
+
+  displayChar();
+  console.log(alphabetArray);
+}
 
 // display first letter
 function displayChar() {
@@ -21,6 +29,7 @@ function displayChar() {
     endGame();
     return;
   }
+  //localhost:3002/image/alphabet/s.svg
   img.src = `image/alphabet/${alphabetArray[0]}.svg`;
 }
 
@@ -44,7 +53,8 @@ function grabInput() {
 function compareAnswer(userInput) {
   console.log(alphabetArray[0]);
   const message = alphabetArray[0] === userInput ? 'you got it' : 'try again';
-  console.log(message);
+
+  feedback.innerHTML = `<p>${message}</p>`;
 
   showNext();
 }
@@ -62,8 +72,7 @@ startBtn.addEventListener('click', function () {
   this.classList.add('hidden');
   gameSection.classList.remove('hidden');
 
-  displayChar();
-  console.log(alphabetArray);
+  startGame();
 });
 
 gameForm.addEventListener('submit', (e) => {
